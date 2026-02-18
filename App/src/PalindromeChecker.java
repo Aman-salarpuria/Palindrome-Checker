bb1932f96248d30c5f5ff182ddea2535c48da616
Aman-salarpuria <amansalarpuria@gmail.com>
2026-02-18 14:54:24 +0530

    did this later as didnt see before

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -2,20 +2,7 @@ import java.util.Scanner;
 public class PalindromeChecker{
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String str = sc.nextLine();
-        String reversed = "";
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
-        }
-
-
-        if (str.equals(reversed)) {
-            System.out.println("The string \"" + str + "\" is a palindrome.");
-        } else {
-            System.out.println("The string \"" + str + "\" is not a palindrome.");
-        }
+        System.out.println("Welcome");
 
     }
 }
