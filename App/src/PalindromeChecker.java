61ce3936b63fc72fa67d1ee27987a78545fccd67
Aman-salarpuria <amansalarpuria@gmail.com>
2026-02-18 16:02:47 +0530

    usecase4character array

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,8 +1,45 @@
+/**
+ * ===========================================================
+ * MAIN CLASS - UseCase4PalindromeCheckerApp
+ * ===========================================================
+ *
+ * Use Case 4: Character Array Based Validation
+ *
+ * Description:
+ * This class validates a palindrome by converting
+ * the string into a character array and comparing
+ * characters using the two-pointer technique.
+ *
+ * At this stage, the application:
+ * - Converts string to char array
+ * - Uses start and end pointers
+ * - Compares characters efficiently
+ * - Displays the result
+ *
+ * This reduces extra memory usage.
+ *
+ * @author Developer
+ * @version 4.0
+ */
+
+
+
 import java.util.Scanner;
 public class PalindromeChecker{
-
     public static void main(String[] args) {
-        System.out.println("Welcome");
-
-    }
+        Scanner sc = new Scanner(System.in);
+        String str=sc.nextLine();
+        char[] strc = str.toCharArray();
+        int n=str.length();
+        int flag=0;
+        for(int i=0;i<n;i++){
+            if(strc[i]==strc[n-1-i]){
+                continue;
+            }else{flag=1;}
+        }
+        if(flag==0){
+            System.out.println("Its a Palindrome");
+        }else{System.out.println("its not a palindrome");
+        }
+        ;}
 }
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Palindrome-Checker
+USE CASE 4
+usage of character array 
+
+# FLOW
+Accept user input using scanner.cc
+
+Convert String to character array using
+
+char[] charArray = str.toCharArray();
+
+Loop through character array and compare 
+
+i and n-1-i elements
+
+using a flag if no match then flag =1 else it stays 0
+
+at the end if flag 0 number is palindrome
